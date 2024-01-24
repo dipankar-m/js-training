a1f547ccd46dfaf88d518c8877b7901b94636300
dipankar-m <dipankar.m@surya-digital.com>
2024-01-24 19:24:19 +0530

    Handle Error in callbacks.js

--- asynchronous/callbacks.js
+++ asynchronous/callbacks.js
@@ -1,29 +1,26 @@
-// Create an Array
-const myNumbers = [4, 1, -20, -7, 5, 9, -6];
+const myNumbers = [4, true, -20, "hi", 5, 9, -6];
 
-// Call removeNeg with a callback
 const posNumbers = pickNumbers(myNumbers, func);
-// (x) => x >= 0
 
 function func(x) {
   // console.log(x);
   return x >= 0;
 }
 
-// console.log(typeof func(2) === "undefined");
+function foo(x) {
+  return true;
+}
 
-// Display Result
 console.log(posNumbers);
 
 function pickNumbers(numbers, callback) {
   const myArray = [];
-  if (typeof func(2) === "undefined") {
-    // Added type check
-    console.log("Callback doesnot return anything");
-    return myArray;
-  }
   for (const x of numbers) {
-    if (callback(x)) {
+    if (
+      callback(x) &&
+      typeof callback(x) != "undefined" &&
+      typeof x == "number"
+    ) {
       myArray.push(x);
     }
   }
